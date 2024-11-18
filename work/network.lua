1d8442ba6d6237eb246366150d784e2078650c47
vhqkze <zqz0601@gmail.com>
2024-11-18 17:21:35 +0800

    feat(network): 自动移除网络代理

---
+++ work/network.lua
@@ -0,0 +1,18 @@
+---@diagnostic disable: lowercase-global
+hs.execute("networksetup -setwebproxystate Ethernet off")
+hs.execute("networksetup -setsecurewebproxystate Ethernet off")
+nc = hs.network.configuration.open()
+nc:monitorKeys("State:/Network/Global/Proxies")
+nc:setCallback(function(a)
+    if hs.network.interfaceName() ~= "Ethernet" then
+        return
+    end
+    local proxy = a:proxies()
+    if proxy.HTTPEnable == 1 then
+        hs.execute("networksetup -setwebproxystate Ethernet off")
+    end
+    if proxy.HTTPSEnable == 1 then
+        hs.execute("networksetup -setsecurewebproxystate Ethernet off")
+    end
+end)
+nc:start()
