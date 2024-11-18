8a0927e86817ae51979947476626c240bc389056
vhqkze <zqz0601@gmail.com>
2024-11-18 16:46:54 +0800

    feat(volume): 调整声音输出设备改变时的音量调整策略

--- plugins/volume.lua
+++ plugins/volume.lua
@@ -1,24 +1,18 @@
 local log = hs.logger.new("volume", "debug")
 hs.audiodevice.watcher.setCallback(function(event)
-    log.i(event, hs.inspect(hs.audiodevice.current()))
     if event == "dOut" then
-        log.i("输出设备改变")
         local current = hs.audiodevice.current(false)
+        log.i("输出设备改变为", current.name)
+        hs.execute("sketchybar --trigger audiodevice_change", true)
         if current.uid == "BuiltInSpeakerDevice" then
-            hs.execute("sketchybar --trigger audiodevice_change", true)
-            if current.muted == false and current.volume > 0 then
-                log.i("重置电脑音量为0")
-                current.device:setOutputVolume(0)
-            end
+            log.i("重置电脑音量为0")
+            current.device:setOutputVolume(0)
         elseif current.name:lower():match("airpods") then
-            log.i("当前是airpods")
-            hs.execute("sketchybar --trigger audiodevice_change", true)
-            if current.muted == false and current.volume > 30 then
-                log.i("重置airpods音量")
-                current.device:setOutputVolume(20)
-            end
-        else
-            hs.execute("sketchybar --trigger audiodevice_change", true)
+            log.i("重置airpods音量")
+            current.device:setOutputVolume(20)
+        elseif current.volume > 30 then
+            log.i("重置音量为30")
+            current.device:setOutputVolume(20)
         end
     end
 end)
