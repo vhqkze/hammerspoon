8c3a9fb3710c7009a470644318f576e9831c4e55
vhqkze <zqz0601@gmail.com>
2024-11-18 16:30:31 +0800

    chore(mouse): 重构调用mouse的相关函数

--- utils/mouse.lua
+++ utils/mouse.lua
@@ -1,18 +1,19 @@
 local M = {}
 local event = hs.eventtap.event
+
+---@enum Rel
 M.rel = {
     WINDOW = "window",
     MOUSE = "mouse",
     START = "start",
-    NIL = nil,
 }
 
--- 各个函数的入参point有三个参数
---  - x: number, 横坐标
---  - y: number, 纵坐标
---  - rel: string, "window": 相对活跃窗口左上角坐标；"mouse": 相对当前鼠标位置坐标; "start": 相对拖拽的起始点坐标（仅拖拽to参数可用）
+---@class Point
+---@field x number
+---@field y number
 
 --- 获取当前focus窗口的位置
+---@return Point|nil
 local function getFocusedWindow()
     local win = hs.window.focusedWindow()
     if win then
@@ -20,79 +21,116 @@ local function getFocusedWindow()
     end
 end
 
---- 根据用户输入的相对坐标，转换为绝对坐标
----@param point table 相对坐标，根据rel判断相对位置
----@return table 绝对坐标
-local function getNewPoint(point)
-    if point.rel == M.rel.WINDOW then
+---根据用户输入的相对坐标，转换为相对屏幕的坐标
+---@param point Point 相对坐标，根据rel判断相对位置
+---@param rel? Rel
+---@return Point|nil 相对当前屏幕的坐标
+local function getNewPoint(point, rel)
+    if rel == nil then
+        return { x = point.x, y = point.y }
+    end
+    if rel == M.rel.WINDOW then
         local topLeft = getFocusedWindow()
         if topLeft then
-            point.x = topLeft.x + point.x
-            point.y = topLeft.y + point.y
-        else
-            hs.alert.show("No focused window!")
+            return { x = topLeft.x + point.x, y = topLeft.y + point.y }
         end
-    elseif point.rel == M.rel.MOUSE then
+    elseif rel == M.rel.MOUSE then
         local mouse_pos = hs.mouse.getRelativePosition()
-        point.x = mouse_pos.x + point.x
-        point.y = mouse_pos.y + point.y
+        return { x = mouse_pos.x + point.x, y = mouse_pos.y + point.y }
     end
-    return { x = point.x, y = point.y }
 end
 
----左键单击
----@param point table
----@class point
----@field x number
----@field y number
----@field rel string nil或空: 绝对坐标, window: 相对活跃窗口左上角坐标；mouse：相对当前鼠标位置坐标
-function M.click(point)
-    local newPoint = getNewPoint(point)
+---leftClick
+---@param point Point
+---@param rel? Rel
+---@param back? boolean 是否返回原点
+function M.click(point, rel, back)
+    local pos_origin = back and hs.mouse.getRelativePosition() or nil
+    local newPoint = getNewPoint(point, rel)
     hs.eventtap.leftClick(newPoint)
+    if back then
+        hs.mouse.setRelativePosition(pos_origin)
+    end
 end
 
----右键单击
----@param point table
-function M.rightClick(point)
-    local newPoint = getNewPoint(point)
+---middleClick
+---@param point Point
+---@param rel? Rel
+---@param back? boolean 是否返回原点
+function M.middleClick(point, rel, back)
+    local pos_origin = back and hs.mouse.getRelativePosition() or nil
+    local newPoint = getNewPoint(point, rel)
+    hs.eventtap.middleClick(newPoint)
+    if back then
+        hs.mouse.setRelativePosition(pos_origin)
+    end
+end
+
+---rightClick
+---@param point Point
+---@param rel? Rel
+---@param back? boolean 是否返回原点
+function M.rightClick(point, rel, back)
+    local pos_origin = back and hs.mouse.getRelativePosition() or nil
+    local newPoint = getNewPoint(point, rel)
     hs.eventtap.rightClick(newPoint)
+    if back then
+        hs.mouse.setRelativePosition(pos_origin)
+    end
 end
 
----左键双击
----@param point table
-function M.doubleClick(point)
-    local newPoint = getNewPoint(point)
+---doubleClick
+---@param point Point
+---@param rel? Rel
+---@param back? boolean 是否返回原点
+function M.doubleClick(point, rel, back)
+    local pos_origin = back and hs.mouse.getRelativePosition() or nil
+    local newPoint = getNewPoint(point, rel)
     local clickState = event.properties.mouseEventClickState
     event.newMouseEvent(event.types.leftMouseDown, newPoint):setProperty(clickState, 1):post()
     event.newMouseEvent(event.types.leftMouseUp, newPoint):setProperty(clickState, 1):post()
     hs.timer.usleep(1000)
     event.newMouseEvent(event.types.leftMouseDown, newPoint):setProperty(clickState, 2):post()
     event.newMouseEvent(event.types.leftMouseUp, newPoint):setProperty(clickState, 2):post()
+    if back then
+        hs.mouse.setRelativePosition(pos_origin)
+    end
 end
 
----移动鼠标
----@param point table
-function M.move(point)
-    local newPoint = getNewPoint(point)
+---move
+---@param point Point
+---@param rel? Rel
+function M.move(point, rel)
+    local newPoint = getNewPoint(point, rel)
     event.newMouseEvent(event.types.mouseMoved, newPoint):post()
 end
 
----拖拽
----@param from table
----@param to table
-function M.drag(from, to)
-    local point_start = getNewPoint(from)
-    local point_end = nil
-    if to.rel == M.rel.START then
-        point_end = { x = to.x + point_start.x, y = to.y + point_start.y }
-    else
-        point_end = getNewPoint(to)
+---drag
+---@param start_pos Point
+---@param start_rel? Rel
+---@param end_pos Point
+---@param end_rel? Rel
+---@param back? boolean 是否返回原点
+function M.drag(start_pos, start_rel, end_pos, end_rel, back)
+    local pos_origin = back and hs.mouse.getRelativePosition() or nil
+    local pos1 = getNewPoint(start_pos, start_rel)
+    if pos1 == nil then
+        return
     end
-    event.newMouseEvent(event.types.leftMouseDown, point_start):post()
-    event.newMouseEvent(event.types.mouseMoved, point_end):post()
+    local pos2 = nil
+    if end_rel == M.rel.START then
+        pos2 = { x = end_pos.x + pos1.x, y = end_pos.y + pos1.y }
+    else
+        pos2 = getNewPoint(end_pos, end_rel)
+    end
+    event.newMouseEvent(event.types.leftMouseDown, pos1):post()
+    event.newMouseEvent(event.types.mouseMoved, pos2):post()
     hs.timer.doAfter(0.01, function()
-        event.newMouseEvent(event.types.leftMouseUp, point_end):post()
+        event.newMouseEvent(event.types.leftMouseUp, pos2):post()
     end)
+    if back then
+        hs.mouse.setRelativePosition(pos_origin)
+    end
 end
 
 return M
