3dde6a6766bd3e85ffd77720ac4f7c52afda639d
vhqkze <zqz0601@gmail.com>
2024-11-18 16:42:01 +0800

    chore(rest_reminder): 重构逻辑，调整提醒策略

--- plugins/rest_reminder.lua
+++ plugins/rest_reminder.lua
@@ -1,12 +1,20 @@
--- 每隔5s检查一下电脑活跃时间
--- 如果活跃时间少于1分钟，说明在工作
--- 如果一直工作30分钟，提示休息
--- 提示一直存在，直到真的休息（活跃时间大于1分钟）
+-- 基于20-20-20规则的用眼休息提醒
+-- 每隔5s检查一下电脑空闲时间
+-- 如果空闲时间少于2分钟，说明在工作
+-- 如果一直工作20分钟，提示休息
+-- 提示一直存在，直到真的休息（空闲时间大于20s）
 -- 监测到真的休息后，提示关闭，重新开始计时
 
-REST_REMINDER = {}
+local log = hs.logger.new("rest", "debug")
 
-REST_REMINDER.last_rest = os.time()
+REST_REMINDER = {
+    rest_time = hs.timer.seconds(20), -- 休息时间
+    work_time = hs.timer.minutes(20), -- 持续工作时间，达到该时间触发休息提醒
+    idle_time = hs.timer.minutes(2), -- 电脑空闲时间，小于该值视为正在工作
+    last_rest = os.time(), -- 上次休息时间
+    show_time = os.time(),
+}
+
 REST_REMINDER.msg = hs.canvas.new({ x = 0, y = 0, w = 0, h = 0 })
 REST_REMINDER.msg[1] = {
     type = "text",
@@ -29,15 +37,32 @@ REST_REMINDER.msg:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
 
 REST_REMINDER.timer = hs.timer.doEvery(5, function()
     if REST_REMINDER.msg:isShowing() == true then
-        if hs.host.idleTime() > hs.timer.minutes(1) then -- 需要休息1分钟
+        if math.min(os.time() - REST_REMINDER.show_time, hs.host.idleTime()) > REST_REMINDER.rest_time then
             REST_REMINDER.last_rest = os.time()
             REST_REMINDER.msg:hide()
+            log.i("rest done, start timer.")
         end
     else
-        if hs.host.idleTime() > hs.timer.minutes(1) then -- 1分钟不间断工作视为高强度工作
+        if hs.host.idleTime() > REST_REMINDER.idle_time then
             REST_REMINDER.last_rest = os.time()
-        elseif os.time() - REST_REMINDER.last_rest > hs.timer.minutes(30) then -- 连续30分钟工作触发提醒
+        elseif os.time() - REST_REMINDER.last_rest > REST_REMINDER.work_time then
             REST_REMINDER.msg:show()
+            REST_REMINDER.show_time = os.time()
+            log.i("show rest reminder")
         end
     end
 end)
+
+DID_LOCK_WATCHER = hs.caffeinate.watcher.new(function(event)
+    if event == hs.caffeinate.watcher.screensDidLock then
+        log.i("screen locked, stop timer.")
+        REST_REMINDER.msg:hide()
+        REST_REMINDER.timer:stop()
+    elseif event == hs.caffeinate.watcher.screensDidUnlock then
+        log.i("screen unlocked, start timer.")
+        REST_REMINDER.last_rest = os.time()
+        REST_REMINDER.msg:hide()
+        REST_REMINDER.timer:start()
+    end
+end)
+DID_LOCK_WATCHER:start()
